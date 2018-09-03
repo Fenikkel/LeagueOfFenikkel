c3e7f60d94d9f80866992d248f5cf402688016f1
Fenikkel <paufenollosa@gmail.com>
2018-09-03 17:08:00 +0200

    cleaning

--- app/src/main/java/com/al286752/fenikkel/leagueoffenikkel/SplashScreen.java
+++ app/src/main/java/com/al286752/fenikkel/leagueoffenikkel/SplashScreen.java
@@ -2,6 +2,7 @@ package com.al286752.fenikkel.leagueoffenikkel;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -40,6 +41,8 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_LOCKED);
 
         mp = MediaPlayer.create(this, R.raw.load);
 
